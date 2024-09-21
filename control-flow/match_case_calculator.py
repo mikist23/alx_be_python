11fdab9fa28dc5bea672c2abbced545f627e2ec6
@Mikist <mikeochango3@gmail.com>
2024-09-22 00:14:07 +0300

    Simple Calculator with Match Case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,9 +1,9 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-opperator = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-match opperator:
+match operation:
     case "+":
         print(f"The result is {num1 + num2}")
     case "-":
