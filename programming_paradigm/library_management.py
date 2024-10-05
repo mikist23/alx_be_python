7f5460ba70cfee77c282c1c11008f65115d654d7
@Mikist <mikeochango3@gmail.com>
2024-10-05 15:50:43 +0300

    Implementing Basic OOP for a Library Management System

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -36,9 +36,8 @@ class Library():
                     book._is_checked_out = True
     
 
-    def return_book(self,title):
+    def return_book(self):
         for book in self._books:
-            if book.title == title:
                 if book._is_checked_out:
                     book._is_checked_out = False
 
